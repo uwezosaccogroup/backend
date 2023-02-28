bcbf058650f6c8a3450fc78cc93ece9462ed4298
margaret manyeki <maggiemanyeki6@gmail.com>
2023-02-28 20:21:25 +0300

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 
-10.times do 
-    User.create(
-        name: Faker::Name.name,
-        email: Faker::Internet.email,
-        phone: Faker::PhoneNumber.cell_phone,
-        password: Faker::Alphanumeric.alphanumeric(number: 4)
-    )
-end
+#10.times do 
+    #User.create(
+     #   name: Faker::Name.name,
+      #  email: Faker::Internet.email,
+       # phone: Faker::PhoneNumber.cell_phone,
+        #password: Faker::Alphanumeric.alphanumeric(number: 4)
+   # )
+#end
 
-accounts ={
-    "user1":{:name =>"John Doe",:balance =>20000}
-}
+#accounts ={
+   # "user1":{:name =>"John Doe",:balance =>20000}
+#}
