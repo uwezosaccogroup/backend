dcbe0eb8cc3577f902ae673bbe1a19bdcac1f038
margaret manyeki <maggiemanyeki6@gmail.com>
2023-02-28 20:17:20 +0300

    added sln

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+
+10.times do 
+    User.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone,
+        password: Faker::Alphanumeric.alphanumeric(number: 4)
+    )
+end
+
+accounts ={
+    "user1":{:name =>"John Doe",:balance =>20000}
+}
