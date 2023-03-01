62c1967080785e12aba40da5697eda580f2ae112
margaret manyeki <maggiemanyeki6@gmail.com>
2023-03-01 22:29:59 +0300

    added route

--- app/controllers/application.controller.rb
+++ app/controllers/application.controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
     
     # Add your routes here
-    get "/" do
-      users = User.all
-      users.to_json()
-      { message: "Good luck with your project!" }.to_json
-    end
-  
+  #shows all users
+  get "/users" do
+    @users = User.all.to_json
   end
+#Shows all accounts
+  get "/accounts" do
+    @account = Account.all.to_json
+  end
+
+  post "/users" do
+    newuser =  User.create("name":params[:name], "email":params[:email],"phone":params[:phone], "location":params[:location])
+    newuser.to_json
+   end
+end
