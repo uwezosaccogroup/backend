b6893af2fd88e6e0653fe2eca431d793f3dea881
keithkiama <keithkiama2003@gmail.com>
2023-03-02 19:29:40 +0300

    edited savings accounts paths

--- db/migrate/20230302090058_create_current_accounts.rb
+++ db/migrate/20230302090058_create_current_accounts.rb
@@ -2,6 +2,7 @@ class CreateCurrentAccounts < ActiveRecord::Migration[6.1]
   def change
     create_table :current_accounts do |t|
       t.string :user_id
+      t.string :user_name
       t.string :account_number
       t.string :date_of_transaction
       t.string :balance
